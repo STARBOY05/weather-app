87ae33aa04f1aac6455799bd4f07f6727e0b2a6f
Alistair Saldanha <shaanalistair47@gmail.com>
2022-07-15 18:24:17 +0530

    Added Responsiveness

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 function WeatherApp() {
     const api = {
-        key: "9c5c683211843126682f3b1ec11d60ae",
+        key: process.env.REACT_APP_API_KEY,
         base: "https://api.openweathermap.org/data/2.5/"
     }
 
@@ -18,13 +18,17 @@ function WeatherApp() {
     //             console.log(result)
     //         })
     // }
+    // const containerBig = () => {
+    //     return ("transition-transform duration-300 ease-in h-50vh")
+    // }
 
     const handleClick = async () => {
         const response = await fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`,)
         const json = await response.json()
         setWeather(json);
         setQuery('');
-        console.log(json)
+        // containerBig();
+        // console.log(json)
     }
 
     const dateBuilder = (d) => {
@@ -39,20 +43,37 @@ function WeatherApp() {
         return `${day} ${date} ${month} ${year}`
     }
 
+
+    // Change background according to weather
+    // const weatherChange = (e) => {
+    //     if (typeof e.main != 'undefined') {
+    //         console.log(e.main.temp);
+    //         if (e.main.temp > 15) {
+    //             return "flex-style flex-col warm";
+    //         }
+    //         else {
+    //             return "flex-style flex-col"
+    //         }
+    //     }
+    //     else {
+    //         return "flex-style flex-col";
+    //     }
+    // }
+
     return (
         // Weather Section
-        <section className='flex justify-center items-center'>
+        <section className="flex-style flex-col">
+            <h1 className='title text-center text-4xl text-[#fff] px-[15px] py-[10px] mb-3'>WEATHER APP</h1>
             {/* Container */}
-            <div className={((typeof (weather.main) === "undefined")) ? ('weather-box max-w-[75%] min-h-[75vh] flex justify-start items-center p-5 flex-col') : ('weather-box max-w-[75%] min-h-[75vh] flex justify-around items-center p-5 flex-col')}>
-                {/* {((typeof (weather.main) != "undefined")) ? } */}
-                <div className='w-full flex justify-center items-start'>
-                    <input type="text" className='py-2 px-5 min-w-[30%] mx-2' placeholder='Search for a place' onChange={(e) => { setQuery(e.target.value) }} value={query} />
-                    <button className='py-2 px-10 bg-[crimson] min-w-[30%] rounded-md' onClick={handleClick}>Fetch</button>
+            <div className={((typeof (weather.main) === "undefined")) ? ('weather-box lg:min-w-[30%] flex-style p-5 flex-col md:min-w-[60%] mt-10') : ('weather-box lg:min-w-[30%] flex justify-around p-5 flex-col md:min-w-[60%] mt-10')}>
+                <div className='flex-style flex-col py-5'>
+                    <input type="text" className='py-2 px-5 mx-2 mb-5 placeholder-black text-center rounded-2xl lg:min-w-[50%] md:w-[80%] text-2xl' placeholder='Search for a place' onChange={(e) => { setQuery(e.target.value) }} value={query} />
+                    <button className='py-2 px-10 bg-[crimson] rounded-md text-white md:text-2xl' onClick={handleClick}>Fetch</button>
                 </div>
                 {((typeof (weather.main) != "undefined")) ? (
-                    <div className='flex justify-around items-center flex-col py-5 text-center'>
+                    <div className='flex justify-around items-center flex-col'>
                         {/* location-box */}
-                        <div>
+                        <div className='my-8 text-center'>
                             {/* location */}
                             < h2 className='text-[#fff] text-5xl font-medium my-2'>{weather.name}, {weather.sys.country}</h2>
                             {/* date */}
@@ -61,13 +82,14 @@ function WeatherApp() {
                         {/* weather-box */}
                         <div className='w-full flex justify-around items-center'>
                             {/* temp */}
-                            <h2 className='temp rounded-2xl px-[15px] py-[25px] text-[#fff] text-7xl font-bold text-center my-3'>{Math.round(weather.main.temp)}°C</h2>
+                            <h2 className='temp rounded-2xl px-[15px] py-[25px] text-[#fff] text-7xl font-bold text-center my-3 sm:text-4xl md:text-6xl'>{Math.round(weather.main.temp)}°C</h2>
                             {/* weather */}
-                            <h3 className='rounded-2xl px-[15px] py-[25px] text-[#fff] text-3xl font-bold text-center'>Sunny</h3>
+                            <h3 className='rounded-2xl px-[15px] py-[25px] text-[#fff] text-4xl font-bold text-center sm:text-2xl md:text-3xl'>{weather.weather[0].main}</h3>
+                            {/* <h3 className='rounded-2xl px-[15px] py-[25px] text-[#fff] text-4xl font-bold text-center sm:text-2xl md:text-3xl'>{weather.wind.speed}</h3> */}
                         </div>
                     </div>
                 ) : ("")}
-            </div >
+            </div>
         </section >
     )
 }
