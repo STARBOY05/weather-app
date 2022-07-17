8265112eb5220d8c99652ab361eb1e0e3b4903dd
Alistair Saldanha <shaanalistair47@gmail.com>
2022-07-17 09:04:36 +0530

    Changes in container size

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -66,12 +66,12 @@ function WeatherApp() {
             <h1 className='title text-center text-4xl text-[#fff] px-[15px] py-[10px] mb-3'>WEATHER APP</h1>
             {/* Container */}
             <div className={((typeof (weather.main) === "undefined")) ? ('weather-box lg:min-w-[30%] flex-style p-5 flex-col md:min-w-[60%] mt-10') : ('weather-box lg:min-w-[30%] flex justify-around p-5 flex-col md:min-w-[60%] mt-10')}>
-                <div className='flex-style flex-col py-5'>
+                <div className='flex-style flex-col py-5 my-4'>
                     <input type="text" className='py-2 px-5 mx-2 mb-5 placeholder-black text-center rounded-2xl lg:min-w-[50%] md:w-[80%] text-2xl' placeholder='Search for a place' onChange={(e) => { setQuery(e.target.value) }} value={query} />
-                    <button className='py-2 px-10 bg-[crimson] rounded-md text-white md:text-2xl' onClick={handleClick}>Fetch</button>
+                    <button className='py-2 px-10 bg-[crimson] rounded-md text-white md:text-2xl mt-3' onClick={handleClick}>Fetch</button>
                 </div>
                 {((typeof (weather.main) != "undefined")) ? (
-                    <div className='flex justify-around items-center flex-col'>
+                    <div className='flex justify-around items-center flex-col my-4'>
                         {/* location-box */}
                         <div className='text-center'>
                             {/* location */}
@@ -80,7 +80,7 @@ function WeatherApp() {
                             <h3 className='text-[#fff] text-3xl italic my-2'>{dateBuilder(new Date())}</h3>
                         </div>
                         {/* weather-box */}
-                        <div className='w-full flex justify-around items-center'>
+                        <div className='w-full flex justify-around items-center my-4'>
                             {/* temp */}
                             <h2 className='temp rounded-2xl px-[15px] py-[25px] text-[#fff] text-7xl font-bold text-center my-3 sm:text-4xl md:text-6xl'>{Math.round(weather.main.temp)}°C</h2>
                             {/* weather */}
